94cad48d1dc4bb74204f80620633822eb0bab57d
Ashish Kr Pal <palkrashish@gmail.com>
2026-05-02 00:30:37 +0530

    feat:- for loop implemented

--- String/firstString.py
+++ String/firstString.py
@@ -8,3 +8,18 @@
 number = '0123456789'
 
 print(number[0])
+
+chai_type =  'Masala'
+chai_variety  = ['Masala', 'Ginger', 'Elachai']
+quantiy = 2
+# injecting variable using {} bracket.
+order = " I will order {} full {} chai " 
+
+print(order.format(quantiy, chai_type))
+
+print(chai_variety)
+
+for letter in chai_type:
+        print(letter)
+
+        
