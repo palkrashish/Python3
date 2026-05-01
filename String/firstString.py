1a20482f57ff7d769769ac1945f31b35932af804
Ashish Kr Pal <palkrashish@gmail.com>
2026-05-02 00:08:42 +0530

    feat:- added some index of number

--- String/firstString.py
+++ String/firstString.py
@@ -1,6 +1,10 @@
-#how to declare single quote
-char = 'Ashish Kr Pal'
-print(char)
-#how to declare double quote string
-char1 ="New Delhi❤"
-print(char1)
+# #how to declare single quote
+# char = 'Ashish Kr Pal'
+# print(char)
+# #how to declare double quote string
+# char1 ="New Delhi❤"
+# print(char1)
+
+number = '0123456789'
+
+print(number[0])
