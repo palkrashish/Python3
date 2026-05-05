30a9f8a64c217f1bd8b4d1d1e50b3136ee0967a3
Ashish Kr Pal <palkrashish@gmail.com>
2026-05-05 23:14:06 +0530

    feat :- added disctionary example

---
+++ Dictinory.py
@@ -0,0 +1,7 @@
+# Example of Dictionary
+from os import process_cpu_count
+chai_type = {"Masala" : "Spicy" , "Ginger": "Zesty", "Green": "Mint"}
+
+# print(chai_type.get("Ginger"))
+chai_type["Green"] = 'Fresh Chai'
+print(chai_type)
