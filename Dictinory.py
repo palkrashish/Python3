07d03d968d0a4087ddc55dccbda279f4c8bf1144
Ashish Kr Pal <palkrashish@gmail.com>
2026-05-05 23:18:42 +0530

    feat :- added for loop and if and else condition as well

--- Dictinory.py
+++ Dictinory.py
@@ -5,3 +5,13 @@ chai_type = {"Masala" : "Spicy" , "Ginger": "Zesty", "Green": "Mint"}
 # print(chai_type.get("Ginger"))
 chai_type["Green"] = 'Fresh Chai'
 print(chai_type)
+
+for chai in chai_type:
+    print(chai, chai_type[chai])
+
+
+
+    if "masala" in chai_type:
+        print("I have Masala chai")
+    else:
+        print("Not a masalal chai")
